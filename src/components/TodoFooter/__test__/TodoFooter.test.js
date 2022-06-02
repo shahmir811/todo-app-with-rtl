992b81dc66488d73e809eba8418dd3e6ca268f52
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2022-06-02 15:22:56 +0500

    Added unit tests on Header, TodoFooter, and TodoList components. Also added integration test on Todo Component

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -10,16 +10,18 @@ const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
 	);
 };
 
-it('should render the correct amount of incomplete tasks', () => {
-	render(<MockTodoFooter numberOfIncompleteTasks={5} />);
-	const pElement = screen.getByText(/5 tasks left/i);
-	expect(pElement).toBeInTheDocument();
-});
+describe('TodoFooter', () => {
+	it('should render the correct amount of incomplete tasks', () => {
+		render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+		const pElement = screen.getByText(/5 tasks left/i);
+		expect(pElement).toBeInTheDocument();
+	});
 
-it('should render "task" when the number of incomplete tasks is one', () => {
-	render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-	const pElement = screen.getByText(/1 task left/i);
-	expect(pElement).toBeInTheDocument();
+	it('should render "task" when the number of incomplete tasks is one', () => {
+		render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+		const pElement = screen.getByText(/1 task left/i);
+		expect(pElement).toBeInTheDocument();
+	});
 });
 
 // it('p element should be truthy when the number of incomplete tasks is one', () => {
